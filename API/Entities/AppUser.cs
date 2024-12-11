740725112798e0c81edb5d1c41978e54404d39da
Yuirice <yulirice@gmail.com>
2024-12-11 22:36:47 +0800

    End of Section 2

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace API.Entities;
+
+public class AppUser
+{
+  public int Id { get; set; }
+
+  public required string UserName { get; set; }
+}
