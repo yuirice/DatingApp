54fa4ade74b89d8fda4c76f282a808fcec2290bd
Yuirice <yulirice@gmail.com>
2025-02-19 23:09:40 +0800

    End of section 6

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -4,7 +4,16 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideToastr } from 'ngx-toastr';
 
 export const appConfig: ApplicationConfig = {
-    providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient(), provideAnimations()],
+    providers: [
+        provideZoneChangeDetection({ eventCoalescing: true }),
+        provideRouter(routes),
+        provideHttpClient(),
+        provideAnimations(),
+        provideToastr({
+            positionClass: 'toast-bottom-right',
+        }),
+    ],
 };
