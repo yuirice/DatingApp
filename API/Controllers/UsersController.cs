fe0c4c53d6ebe13e3b35adba1f7dfbc360ee5fc8
Raymond <yulirice@gmail.com>
2025-07-06 10:46:52 +0800

    End of section 10

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,3 +1,4 @@
+using System.Security.Claims;
 using API.Data;
 using API.DTOs;
 using API.Entities;
@@ -10,24 +11,42 @@ using Microsoft.EntityFrameworkCore;
 namespace API.Controllers;
 
 [Authorize]
-public class UsersController(IUserRepository userRepository) : BaseApiController
+public class UsersController(IUserRepository userRepository, IMapper mapper) : BaseApiController
 {
   [HttpGet]
-   public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
-   {
+  public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
+  {
     var users = await userRepository.GetMembersAsync();
- 
-    return Ok(users);
-   }
 
-   [HttpGet("{username}")]  // api/users/2
-   public async Task<ActionResult<MemberDto>> GetUser(string username)
-   {
+    return Ok(users);
+  }
+
+  [HttpGet("{username}")]  // api/users/2
+  public async Task<ActionResult<MemberDto>> GetUser(string username)
+  {
     var user = await userRepository.GetMemberAsync(username);
 
-    if(user == null) return NotFound();
+    if (user == null) return NotFound();
 
     return user;
-   }
+  }
+
+  [HttpPut]
+  public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+  {
+    var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+    if (username == null) return BadRequest("No username found in token");
+
+    var user = await userRepository.GetUserByUsernameAsync(username);
+
+    if (user == null) return BadRequest("Could not find user");
+
+    mapper.Map(memberUpdateDto, user);
+
+    if (await userRepository.SaveAllAsync()) return NoContent();
+
+    return BadRequest("Failed to update user");
+  }
 }
 
--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -10,9 +10,10 @@ public class AutoMapperProfiles : Profile
     public AutoMapperProfiles()
     {
         CreateMap<AppUser, MemberDto>()
-           .ForMember(d => d.Age, o => o.MapFrom( s => s.DateOfBirth.CalculateAge() ))
-           .ForMember(d => d.PhotoUrl, o => 
-           o.MapFrom( s => s.Photos.FirstOrDefault( x => x.IsMain)!.Url));
+           .ForMember(d => d.Age, o => o.MapFrom(s => s.DateOfBirth.CalculateAge()))
+           .ForMember(d => d.PhotoUrl, o =>
+           o.MapFrom(s => s.Photos.FirstOrDefault(x => x.IsMain)!.Url));
         CreateMap<Photo, PhotoDto>();
+        CreateMap<MemberUpdateDto, AppUser>();
     }
 }
